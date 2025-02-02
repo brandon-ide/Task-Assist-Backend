0f0d3454a47e95be292b1dde12645bc3e333552f
David Dutton <striveforsuccess365247@gmail.com>
2025-02-02 17:03:12 -0500

    fixing dueDate functionality and using Morgan for tracking requests

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,6 @@
 // require the express module
 import express from "express";
-
+import morgan from "morgan";
 // require the cors module
 import cors from "cors";
 
@@ -9,14 +9,12 @@ import taskRouter from "./routes/taskRouter";
 // creates an instance of an Express server
 const app = express();
 
-
 // enable Cross Origin Resource Sharing so this API can be used from web-apps on other domains
 app.use(cors());
-
+app.use(morgan('dev'));
 
 // allow POST and PUT requests to use JSON bodies
 app.use(express.json());
-
 app.use("/", taskRouter);
 
 // define the port
